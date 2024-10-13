b714ad835733ce7e939b37fb4172508121830142
DinaMazur <146865201+DinaMazur@users.noreply.github.com>
2024-10-13 14:10:29 +0300

    Add files via upload

---
+++ js/hw1.js
@@ -0,0 +1,29 @@
+const Age = "15";
+console.log(Age);
+
+const Name = "Dina";
+console.log(Name);
+
+const isStudent = 1
+const student = true;
+console.log(student);
+
+const isString = "Коли ти чекаєш, значить ти маєш, на кого чекати";
+console.log(isString);
+
+const myNumber = 1;
+const sum = myNumber + 10;
+console.log(sum);
+
+const myNull = "null"
+console.log(myNull);
+
+const yourName = prompt("Введіть ім'я користувача");
+console.log(yourName);
+
+
+const isComing = confirm("Привіт ");
+console.log(isComing);
+
+
+
